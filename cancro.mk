adc440101874bcb5017981eda54cc23e056697b1
DrRamm <kotsareu@gmail.com>
2017-07-10 22:38:05 +0300

    cancro: add ril configs

--- cancro.mk
+++ cancro.mk
@@ -223,6 +223,12 @@ PRODUCT_PACKAGES += \
     libnl_2 \
     libbson
 
+# RIL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
